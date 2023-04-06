9580a247adeae5b0052a7114e6be6baa6b510130
Ignazio <ignazioandsperandeo@gmail.com>
2023-04-06 17:33:19 +0200

    .

--- voltmetro arduino/CODICE_VOLTMETRO_ARDUINO/CODICE_VOLTMETRO_ARDUINO.ino
+++ voltmetro arduino/CODICE_VOLTMETRO_ARDUINO/CODICE_VOLTMETRO_ARDUINO.ino
@@ -3,23 +3,35 @@
 #define VOLT_ARD 5.0
 #define R1 10000.0
 #define R2 2000.0
-
+#define PIN_Y 6
+#define PIN_X 7
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 const float inverse = (R1+R2)/R2;
 float analog_value;
 
 void setup(){
+  pinMode(PIN_X, OUTPUT);
+  pinMode(PIN_Y, OUTPUT);
   Serial.begin(9600);
-		// lcd.begin(16, 2);
-		// lcd.print("VOLTMETRO");
+  lcd.begin(16, 2);
+  lcd.print("BY JIM-BUG");
+  delay(300);
+  lcd.clear();
+  lcd.print("VOLTMETRO");
 }
 void loop(){
+  lcd.setCursor(0, 1);
+  digitalWrite(PIN_Y, HIGH);
 	analog_value = analogRead(A4);
 	float vout = ((analog_value*VOLT_ARD)/1023.0) *	inverse;
-	
-
-	Serial.print(vout);
-  Serial.print("\n");
+  lcd.print("VOLT: ");
+	lcd.print(vout);
+  lcd.print("V");
+  if (vout >= 28){
+    digitalWrite(PIN_X, HIGH);
+    delay(500);
+    digitalWrite(PIN_X, LOW);
+  }
 
 	delay(200);
-
 }
